1ba675850008d648837c3c9c5f011d4d448d575a
Ilya Burakov <burakov.ilia@gmail.com>
2019-09-25 01:12:23 +0800

    fix Telegram provider

--- Emb.TargetProvider.Telegram/TelegramTargetProvider.cs
+++ Emb.TargetProvider.Telegram/TelegramTargetProvider.cs
@@ -5,27 +5,31 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Composition;
 using System.Threading.Tasks;
+using System.Web;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Emb.TargetProvider.Telegram
 {
     [Export(typeof(ITargetProvider))]
     public class TelegramTargetProvider : ITargetProvider
     {
-        private TelegramBotClientFactory _telegramBotClientFactory = new TelegramBotClientFactory();
+        private readonly TelegramBotClientFactory _telegramBotClientFactory = new TelegramBotClientFactory();
 
         public async Task SendAsync(ILoggerFactory loggerFactory, IConfigurationRoot configurationRoot, string endpointOptionsString, string text)
         {
             var providerSettings = configurationRoot.GetSection(GetType().Name).Get<ProviderSettings>();
             var telegramBotClient = _telegramBotClientFactory.CreateTelegramBotClient(providerSettings);
 
+            text = HttpUtility.HtmlEncode(text) ?? string.Empty;
+            
             const int maxTelegramStringLength = 4095;
             if (text.Length > maxTelegramStringLength)
             {
                 text = text.Substring(0, maxTelegramStringLength);
             }
 
-            await telegramBotClient.SendTextMessageAsync(new ChatId(endpointOptionsString), text);
+            await telegramBotClient.SendTextMessageAsync(new ChatId(endpointOptionsString), text, ParseMode.Html);
         }
 
         public Type GetEndpointOptionsType()
