cfd9b8d1cf28154ab023e793ae5e1a32f4e53a08
Ilia Burakov <burakov.ilia@gmail.com>
2021-09-06 14:30:31 +0800

    use vk api v5.131

--- Emb.DataSourceProvider.VkFeed/Abstractions/IVkApi.cs
+++ Emb.DataSourceProvider.VkFeed/Abstractions/IVkApi.cs
@@ -6,10 +6,10 @@ namespace Emb.DataSourceProvider.VkFeed.Abstractions
 {
     public interface IVkApi
     {
-        [Get("/method/newsfeed.get?return_banned=0&count=100&filters=post&access_token={accessToken}&v=5.80")]
+        [Get("/method/newsfeed.get?return_banned=0&count=100&filters=post&access_token={accessToken}&v=5.131")]
         Task<VkNewsFeedGetResponse> GetFeedFirstPage(string accessToken);
 
-        [Get("/method/newsfeed.get?return_banned=0&count=100&filters=post&start_from={startFrom}&access_token={accessToken}&v=5.80")]
+        [Get("/method/newsfeed.get?return_banned=0&count=100&filters=post&start_from={startFrom}&access_token={accessToken}&v=5.131")]
         Task<VkNewsFeedGetResponse> GetFeedNextPage(string accessToken, string startFrom);
     }
 }
